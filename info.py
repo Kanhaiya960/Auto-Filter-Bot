6a87756f885155bdb63e3a30c3b966ea3da4d066
Kanhaiya960 <168913838+Kanhaiya960@users.noreply.github.com>
2024-12-30 19:02:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,28 +17,28 @@ def is_valid_ip(ip):
     return re.match(ip_pattern, ip) is not None
 
 # Bot information
-API_ID = environ.get('API_ID', '11973721')
+API_ID = environ.get('API_ID', '29265798')
 if len(API_ID) == 0:
     print('Error - API_ID is missing, exiting now')
     exit()
 else:
     API_ID = int(API_ID)
-API_HASH = environ.get('API_HASH', '5264bf4663e9159565603522f58d3c18')
+API_HASH = environ.get('API_HASH', '9dd673fa7291fb5a954902ea10fc8cb5')
 if len(API_HASH) == 0:
     print('Error - API_HASH is missing, exiting now')
     exit()
-BOT_TOKEN = environ.get('BOT_TOKEN', '7339692339:AAFAOyRX0EdURGDLkENqT4rIBt_rfQHWCtY')
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
 if len(BOT_TOKEN) == 0:
     print('Error - BOT_TOKEN is missing, exiting now')
     exit()
-PORT = int(environ.get('PORT', '80'))
+PORT = int(environ.get('PORT', '8080'))
 USER_STRING_SESSION = environ.get('USER_STRING_SESSION', '1BVtsOKEBu7SQF8Ck5YduBg9nXiMW9RHtI7x54sGunU6baup5j7Q3oPCF90PFNGjAY0Rh0X6mm1-GwMixKrb7wtgoHsX-j-ZCxYAW8OXnuAGUhbAC0Osff7I0mOxFT-Ropf579sHfd3dK87V_G9GZh84NwbloLdWCrFstAZdjeweBLlUpdpWQsvu11yWD63iN7d1VxmiV1-mo1A7LPe4zPz41fd057m9r-xpnd_M1bAEe5SAWArq_h4IPhWNkakvMxf5CrKg7V7kVK3AyqhDxe5HEE0_nxFSDXXxiBfj9G6k8LwXpnjy3MXG5d5JbWw9Yhj3REtsLRRAkImh7l7sMxOUd_-AMyR8=')
 
 # Bot pics
-PICS = (environ.get('PICS', 'https://envs.sh/4kP.jpg')).split()
+PICS = (environ.get('PICS', 'https://envs.sh/8NH.jpg')).split()
 
 # Bot Admins
-ADMINS = environ.get('ADMINS', '1391556668')
+ADMINS = environ.get('ADMINS', '6552970915')
 if len(ADMINS) == 0:
     print('Error - ADMINS is missing, exiting now')
     exit()
@@ -46,21 +46,21 @@ else:
     ADMINS = [int(admins) for admins in ADMINS.split()]
 
 # Channels
-INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '-1002267578638 -1001816164988').split()]
+INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '-1002457424722 -1002439854017').split()]
 if len(INDEX_CHANNELS) == 0:
     print('Info - INDEX_CHANNELS is empty')
-LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1001821439025')
+LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1002237007652)
 if len(LOG_CHANNEL) == 0:
     print('Error - LOG_CHANNEL is missing, exiting now')
     exit()
 else:
     LOG_CHANNEL = int(LOG_CHANNEL)
-MOVIE_UPDATE_CHANNEL = [int(movie_update_channel) if movie_update_channel.startswith("-") else movie_update_channel for movie_update_channel in environ.get('MOVIE_UPDATE_CHANNEL', '-1002244141688 -1001715180239 -1001589399161 -1001139111796 -1002097728232').split()]
-FORCE_SUB = [int(force_sub) if force_sub.startswith("-") else force_sub for force_sub in environ.get('FORCE_SUB', '-1002008853384 -1001589399161').split()]
+MOVIE_UPDATE_CHANNEL = [int(movie_update_channel) if movie_update_channel.startswith("-") else movie_update_channel for movie_update_channel in environ.get('MOVIE_UPDATE_CHANNEL', '-1002090374492').split()]
+FORCE_SUB = [int(force_sub) if force_sub.startswith("-") else force_sub for force_sub in environ.get('FORCE_SUB', '-1002090374492').split()]
 if len(FORCE_SUB) == 0:
     print('Info - FORCE_SUB is empty')
 # For Forwarding 
-SOURCE_CHANNELS1 = int(environ.get('SOURCE_CHANNELS1', '-1001822541447')) # 1TamilBlasters
+SOURCE_CHANNELS1 = int(environ.get('SOURCE_CHANNELS1', '-1002439854017')) # 1TamilBlasters
 SOURCE_CHANNELS2 = int(environ.get('SOURCE_CHANNELS2', '-1001864825324')) # 1TamilMV
 SOURCE_CHANNELS3 = int(environ.get('SOURCE_CHANNELS3', '-1001822541447')) # 1TamilBlasters
 SOURCE_CHANNELS4 = int(environ.get('SOURCE_CHANNELS4', '-1001864825324')) # 1TamilMV
@@ -69,7 +69,7 @@ SOURCE_CHANNELS6 = int(environ.get('SOURCE_CHANNELS6', '-1001864825324')) # 1Tam
 SOURCE_CHANNELS7 = int(environ.get('SOURCE_CHANNELS7', '2082930520'))
 
 # support group
-SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-1001895961046')
+SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-1002256368276')
 if len(SUPPORT_GROUP) == 0:
     print('Error - SUPPORT_GROUP is missing, exiting now')
     exit()
@@ -85,12 +85,12 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Links
-SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/Star_Bots_Tamil_Support')
-OWNER_USERNAME = environ.get("OWNER_USERNAME", "https://t.me/U_Karthik")
-UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/DP_BOTZ')
-FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/Movies_Dayz')
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/How_downlode_dpbots/22")
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/How_downlode_dpbots/22")
+SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/UpperZoneBot')
+OWNER_USERNAME = environ.get("OWNER_USERNAME", "https://t.me/Kanus_Network")
+UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/Movies_4_Download')
+FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/Movies_4_Download')
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/Movies_4_Download/22")
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/Movies_4_Download/22")
 
 # Bot settings
 DELETE_TIME = int(environ.get('DELETE_TIME', 3600)) # Add time in seconds
@@ -108,8 +108,8 @@ INDEX_EXTENSIONS = [extensions.lower() for extensions in environ.get('INDEX_EXTE
 PM_FILE_DELETE_TIME = int(environ.get('PM_FILE_DELETE_TIME', '3600'))
 
 # boolean settings
-IS_PM_SEARCH = is_enabled('IS_PM_SEARCH', False)
-IS_VERIFY = is_enabled('IS_VERIFY', True)
+IS_PM_SEARCH = is_enabled('IS_PM_SEARCH', True)
+IS_VERIFY = is_enabled('IS_VERIFY', False)
 IS_SEND_MOVIE_UPDATE = is_enabled('IS_SEND_MOVIE_UPDATE', True) # Don't Change It ( If You Want To Turn It On Then Turn It On By Commands) We Suggest You To Make It Turn Off If You Are Indexing Files First Time.
 AUTO_DELETE = is_enabled('AUTO_DELETE', True)
 WELCOME = is_enabled('WELCOME', False)
@@ -122,21 +122,21 @@ SPELL_CHECK = is_enabled("SPELL_CHECK", True)
 SHORTLINK = is_enabled('SHORTLINK', False)
 
 #premium info
-PAYMENT_QR = environ.get('PAYMENT_QR', 'https://envs.sh/4UC.jpg')
-OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'starbotstamil@oksbi')
+PAYMENT_QR = environ.get('PAYMENT_QR', 'https://envs.sh/8NH.jpg')
+OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'Contact At @rj_09_kanhaiya')
 
 # RSS Feed URL
 TAMILMV = environ.get("TMV", "https://www.1tamilmv.uno/")
 TAMILBLAST = environ.get("TB", "https://www.1tamilblasters.party/")
 TAMILROCKERS = environ.get("TR", "https://www.2tamilrockers.com/")
 # log channel list
-TAMILMV_LOG = int(environ.get("TMV_LOG", -1001864825324))
-TAMILBLAST_LOG = int(environ.get("TB_LOG", -1001822541447))
-TAMILROCKERS_LOG = int(environ.get("TR_LOG", -1002056074553))
+TAMILMV_LOG = int(environ.get("TMV_LOG", -1002237007652))
+TAMILBLAST_LOG = int(environ.get("TB_LOG", -1002237007652))
+TAMILROCKERS_LOG = int(environ.get("TR_LOG", -1002237007652))
 
 # for stream
 IS_STREAM = is_enabled('IS_STREAM', True)
-BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1002386293179")
+BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1002410949273")
 if len(BIN_CHANNEL) == 0:
     print('Error - BIN_CHANNEL is missing, exiting now')
     exit()
